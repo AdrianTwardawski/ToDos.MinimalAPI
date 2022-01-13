c2322cf8a47cf0e2727c7cad5e24f37bf1fed932
adriantwardy <adriantwardawski@gmail.com>
2022-01-13 18:42:46 +0100

    Authorization and Authentication

--- ToDos.MinimalAPI/ToDo/ToDoRequests.cs
+++ ToDos.MinimalAPI/ToDo/ToDoRequests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ToDos.MinimalAPI;
 
@@ -8,12 +9,14 @@ public static class ToDoRequests
     { 
         app.MapGet("/todos", ToDoRequests.GetAll)
             .Produces<List<ToDo>>()
-            .WithTags("To Dos");
+            .WithTags("To Dos")
+            .RequireAuthorization();
 
         app.MapGet("/todos/{id}", ToDoRequests.GetById)
             .Produces<ToDo>()
             .Produces(StatusCodes.Status404NotFound)
-            .WithTags("To Dos");
+            .WithTags("To Dos")
+            .AllowAnonymous();
 
         app.MapPost("/todos", ToDoRequests.Create)
             .Produces<ToDo>(StatusCodes.Status201Created)
@@ -53,6 +56,7 @@ public static class ToDoRequests
         return Results.Ok(todo);
     }
 
+    [Authorize]
     public static IResult Create(IToDoService service, ToDo toDo)
     {     
         service.Create(toDo);
